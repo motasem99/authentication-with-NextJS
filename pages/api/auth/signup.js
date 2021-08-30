2d3cda9ff7d6562c70263fbc7c3e2d533a5c28a0
mutasem <motasemimad10@gmail.com>
2021-08-30 16:53:03 +0300

    sending signup request from the frontend

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,32 +1,34 @@
 import { hashPassword } from '../../../lip/auth';
 import { connectToDatabase } from '../../../lip/db';
 
-function handler(req, res) {
-  const data = req.body;
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+    const { email, password } = data;
 
-  const { email, password } = data;
+    if (
+      !email ||
+      !email.includes('@') ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({
+        message:
+          'Invalid input - password should also be at least 7 characters',
+      });
+      return;
+    }
 
-  if (
-    !email ||
-    !email.includes('@') ||
-    !password ||
-    password.trim().length < 7
-  ) {
-    res.status(422).json({
-      message: 'Invalid input - password should also be at least 7 characters',
+    const client = await connectToDatabase();
+    const db = client.db();
+    const hashedPassword = await hashPassword(password);
+    const result = await db.collection('users').insertOne({
+      email,
+      password: hashedPassword,
     });
-    return;
+
+    res.status(201).json({ message: 'Created user!' });
   }
-
-  const client = await connectToDatabase();
-  const db = client.db();
-  const hashedPassword = hashPassword(password);
-  const result = await db.collection('users').insertOne({
-    email,
-    password: hashedPassword,
-  });
-
-  res.status(201).json({ message: 'Created user!' });
 }
 
 export default handler;
