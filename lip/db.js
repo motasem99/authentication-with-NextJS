6d5cf0820df73e7aa667b63b397c26ba3d76fbdf
mutasem <motasemimad10@gmail.com>
2021-08-30 16:14:45 +0300

    adding a user signup API route

---
+++ lip/db.js
@@ -0,0 +1,9 @@
+const { MongoClient } = require('mongodb');
+
+export async function connectToDatabase() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://mutasem-NextjsProject :2741999@cluster0.6sm4t.mongodb.net/auth-demo?retryWrites=true&w=majority'
+  );
+
+  return client;
+}
