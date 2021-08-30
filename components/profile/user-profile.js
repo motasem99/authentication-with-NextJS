9ee2f46afc460a8e0174813ccb3d527ca6341020
mutasem <motasemimad10@gmail.com>
2021-08-30 20:04:44 +0300

    sending a change password request from the frontend

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 // import { useSession, getSession } from 'next-auth/client';
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 function UserProfile() {
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
 
   // useEffect(() => {
   //   getSession().then((session) => {
@@ -20,10 +20,23 @@ function UserProfile() {
   //   return <p className={classes.profile}>Loading...</p>;
   // }
 
+  const changePasswordHandler = async (passwordData) => {
+    const response = await fetch('/api/user/change-password', {
+      method: 'PATCH',
+      body: JSON.stringify(passwordData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changePasswordHandler} />
     </section>
   );
 }
